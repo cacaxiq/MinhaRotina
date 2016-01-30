f3c23867623375251c823027b2baf500fb6527d7
Carlos Henrique Rocha Gabriel <carloshenriquerg@gmail.com>
2016-01-30 17:55:11 -0200

    Inclusao pastas de Pages e Models

---
+++ Pages/RegisterPlace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MInhaRotina
+{
+	public class RegisterPlace : ContentPage
+	{
+		public RegisterPlace ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
